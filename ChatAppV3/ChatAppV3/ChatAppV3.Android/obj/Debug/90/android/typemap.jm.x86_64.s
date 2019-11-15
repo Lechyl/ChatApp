2148be9833cc3ac7d8331be4423173b94d070ed5
Long Yong Ly <long0020@edu.sde.dk>
2019-11-15 11:32:41 +0100

    First Init

---
+++ ChatAppV3/ChatAppV3/ChatAppV3.Android/obj/Debug/90/android/typemap.jm.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 87ada168dd67ee2cc56a9b089522710bf47dc3ef */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1284
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 336409
+	.include	"typemap.jm.inc"
