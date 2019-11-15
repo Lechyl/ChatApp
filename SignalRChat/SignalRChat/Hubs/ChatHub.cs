7c91bbbf13885ac7bad9a8adec592bc6c6584986
Long Yong Ly <long0020@edu.sde.dk>
2019-11-15 11:41:11 +0100

    Deleted Files

---
+++ SignalRChat/SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinChat(string user)
+        {
+            string cid = Context.ConnectionId;
+            
+            await Clients.All.SendAsync("JoinChat", user);
+        }
+
+        public async Task LeaveChat(string user)
+        {
+            await Clients.All.SendAsync("LeaveChat", user);
+        }
+
+        public async Task SendMessageAll(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendMessageClient(string user, string message, string toUser)
+        {
+            await Clients.Client(toUser).SendAsync("ReceiveMessage",user,message);
+        }
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+
+    }
+}
---
+++ SignalRChat/SignalRChat/Startup.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SignalRChat.Hubs;
+
+
+namespace SignalRChat
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors(cors =>
+            {
+                cors.AllowAnyHeader();
+                cors.AllowAnyOrigin();
+                cors.AllowAnyMethod();
+            });
+
+            app.UseWebSockets();
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("/chathub");
+            });
+
+
+        }
+    }
+}
